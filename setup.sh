36be21b3f1f55b27a90ba58a5a95c356d4e58392
Sishaar Rao <sishaar@gmail.com>
2017-04-18 18:05:14 -0400

    setup executable

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ git add --all
 git commit -m "Initial Commit"
 git push heroku master
 heroku ps:scale worker=1
-echo "Setup complete. Run 'heroku ps' after some time. If you see 'worker: crashed' refer to README. Otherwise, Tweet@Trump is up!"
+echo "Setup complete. Run 'heroku ps' after some time. If you see 'worker: crashed' refer to README. Otherwise, FancyTweeter is up!"
