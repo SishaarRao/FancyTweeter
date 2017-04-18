c4f0820c1a974a2fc19acf61f0314f6a5948a971
Sishaar Rao <sishaar@gmail.com>
2017-04-18 11:54:37 -0400

    added input for twitter keys

--- secret.py
+++ secret.py
@@ -1,3 +1,13 @@
 # Go to http://www.dictionaryapi.com and create an account.
 # Generate a Thesauras Key and insert it here
 THESAURAS_KEY = 'INSERT YOUR THESAURAS KEY HERE'
+
+# Go to http://dev.twitter.com and create an app.
+# The consumer key and secret will be generated for you after
+CONSUMER_KEY = 'INSERT YOUR CONSUMER KEY HERE'
+CONSUMER_SECRET = 'INSERT YOUR SECRET CONSUMER KEY HERE'
+
+# After the step above, you will be redirected to your app's page.
+# Create an access token under the the "Your access token" section
+ACCESS_KEY = 'INSERT YOUR ACCESS KEY HERE'
+ACCESS_SECRET = 'INSERT YOUR SECRET ACCESS KEY HERE'
