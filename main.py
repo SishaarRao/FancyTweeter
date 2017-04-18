b87e7db1bdd516b80df70d742efc691790f191de
Sishaar Rao <sishaar@gmail.com>
2017-04-18 11:54:55 -0400

    added shell for twitter UserStream from Tweet@Trump

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from sishaarSecret import THESAURAS_KEY
-# from secret import THESAURAS_KEY
+from sishaarSecret import THESAURAS_KEY, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
+# from secret import THESAURAS_KEY, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
 import urllib.request
 import re
 import lxml.etree as ET
 from stop_words import get_stop_words
+import tweepy, time, sys
 
 def parse(KEYWORD):
     # Request to the Thesauras API
@@ -28,18 +29,51 @@ def parse(KEYWORD):
         # Not a valid entry
         return KEYWORD
 
-# Separate the input into words
-strInput = "I hate Donald Trump"
-strInput = re.compile('[^a-zA-Z]').sub(' ', strInput).split()
-
-# Loop through the input and parse each word, disregarding articles or stop words
-stop_words = get_stop_words('english')
-articles = open("common_articles.txt").read().split()
-print(stop_words)
-for word in strInput:
-    if word not in stop_words and word not in articles:
-        print(parse(word))
-    else:
-        print(word)
-        
+def tweet(strInput):
+    # Separate the input into words
+    strInput = "I hate Donald Trump"
+    strInput = re.compile('[^a-zA-Z]').sub(' ', strInput).split()
     
+    # Loop through the input and parse each word, disregarding articles or stop words
+    stop_words = get_stop_words('english')
+    articles = open("common_articles.txt").read().split()
+    print(stop_words)
+    for word in strInput:
+        if word not in stop_words and word not in articles:
+            print(parse(word))
+        else:
+            print(word)
+
+# Create client
+try:
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    
+    client = tweepy.API(auth)
+    if not client.verify_credentials():
+        raise tweepy.TweepError
+
+    USER = client.get_user(USERNAME)
+    ID = USER.id_str
+except tweepy.TweepError as e:
+    print('ERROR : connection failed. Check your OAuth keys.')
+    sys.exit(0)
+else:
+    print('Connected as @{}, you can start to tweet !'.format(client.me().screen_name))
+    client_id = client.me().id
+    
+
+class MyStreamListener(tweepy.StreamListener):
+    def on_status(self, status):
+        print(status.text)
+        if not "@"+USERNAME in status.text:
+            client.update_status("@" + (USERNAME) + " "  + message, status.id_str)
+    def on_error(self, status_code):
+        if status_code == 420:
+            return False
+
+# Create Stream Listener
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth = client.auth, listener=myStreamListener)
+
+
